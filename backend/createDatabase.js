8e6fd585e2cc1523d8375f593eb2d6092920807c
Henry Pick <1henrypick@gmail.com>
2024-02-01 15:12:46 -0800

    node_modules remove

--- backend/createDatabase.js
+++ backend/createDatabase.js
@@ -1,26 +1,26 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 // Create a connection to the database
 const connection = mysql.createConnection({
-  host: 'g84t6zfpijzwx08q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  user: 'ajezug5c2jtz5d4z', // replace with your MySQL username
-  password: 'n34zb61zk2yfdhlm', // replace with your MySQL password
+  host: "g84t6zfpijzwx08q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "ajezug5c2jtz5d4z", // replace with your MySQL username
+  password: "n34zb61zk2yfdhlm", // replace with your MySQL password
 });
 
 connection.connect((err) => {
   if (err) {
-    return console.error('Error: ' + err.message);
+    return console.error("Error: " + err.message);
   }
-  console.log('Connected to the MySQL server.');
+  console.log("Connected to the MySQL server.");
 
   // Create the 'signup' database
   const createDatabaseQuery = `CREATE DATABASE IF NOT EXISTS c1gr4bjdqxa06vz0`;
   connection.query(createDatabaseQuery, (err, results) => {
     if (err) throw err;
     console.log("Database 'c1gr4bjdqxa06vz0' created or already exists.");
-    
+
     // Use the 'signup' database
-    connection.changeUser({database : 'c1gr4bjdqxa06vz0'}, (err) => {
+    connection.changeUser({ database: "c1gr4bjdqxa06vz0" }, (err) => {
       if (err) throw err;
 
       // Create the 'users' table
@@ -50,8 +50,6 @@ connection.connect((err) => {
             team2logo TEXT NOT NULL,
             game_time TIME NOT NULL,
             winner VARCHAR(255)
-
-
           )`;
         connection.query(createGamesTableQuery, (err, results) => {
           if (err) throw err;
