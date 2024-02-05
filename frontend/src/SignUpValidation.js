5a3131c6d7d6c07e71d429fbe87bfc22c52e52a0
Henry Pick <1henrypick@gmail.com>
2024-02-05 10:03:40 -0800

    login validation message

--- frontend/src/SignUpValidation.js
+++ frontend/src/SignUpValidation.js
@@ -1,36 +1,31 @@
-function Validation(values){
-    
-    let error = {}
-    const email_patter = /^\S+@\S+\.\S+$/
-    const password_pattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+function Validation(values) {
+  let error = {};
+  const email_patter = /^\S+@\S+\.\S+$/;
+  const password_pattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
-    if(values.name === ""){
-        error.name = "name should not be empty"
-    }
-    else{
-        error.name = ""
-    }
+  if (values.name === "") {
+    error.name = "name should not be empty";
+  } else {
+    error.name = "";
+  }
 
-    if(values.email === ""){
-        error.email = "name should not be empty"
-    }
-    else if(!email_patter.test(values.email)){
-        error.email = "email did not match"
-    }
-    else{
-        error.email = ""
-    }
+  if (values.email === "") {
+    error.email = "name should not be empty";
+  } else if (!email_patter.test(values.email)) {
+    error.email = "email did not match";
+  } else {
+    error.email = "";
+  }
 
-    if(values.password === ""){
-        error.password = "password should not be empty"
-    }
-    else if(!password_pattern.test(values.password)){
-        error.password = "password did not match"
-    }
-    else{
-        error.password = ""
-    }
-    
-    return error;
+  if (values.password === "") {
+    error.password = "password should not be empty";
+  } else if (!password_pattern.test(values.password)) {
+    error.password =
+      "Passwords should be at least 8 characters and have both numbers and characters";
+  } else {
+    error.password = "";
+  }
+
+  return error;
 }
 export default Validation;
